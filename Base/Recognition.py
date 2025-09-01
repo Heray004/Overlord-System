d95e379ae8f9e96aa6395fba41a8bf8e76a92fb3
Heray004 <igorek_004@mail.ru>
2025-09-01 10:57:03 +0300

    Overlord System v1.8

--- Base/Recognition.py
+++ Base/Recognition.py
@@ -11,7 +11,7 @@ from OverlordAccess.HomeAssistant.OverlordAccessHA import OverlordAccessHA
 # -------------------------------------------
 
 
-class Recognition(WordBase, BaseFunction, OverlordControlIR, OverlordControlZigbee, OverlordAccessHA):
+class Recognition(BaseFunction, OverlordControlIR, OverlordControlZigbee, OverlordAccessHA):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         with open(r"DecisionModel/Overlord_model_v0.1.pkl", "rb") as model_file:
@@ -26,83 +26,22 @@ class Recognition(WordBase, BaseFunction, OverlordControlIR, OverlordControlZigb
         ha.start()
         sleep(0.1)
 
-    def list_recognition(self, text):
-        # --------------------------------------------------------------------------------------------------------------
-        for k, v in self.word_base['other'].items():
-            if text in v:
-                try:
-                    getattr(self, k[1])()
-                except AttributeError:
-                    print("комманда распознанна, но не имеет функции")
-
-        # --------------------------------------------------------------------------------------------------------------
-        for name in self.word_base["name"]:
-            if name in text:
-                self.name_flag = True
-                self.name_score = 0
-                if text.startswith(name):
-                    text = text.replace(name + " ", "")
-                elif text.endswith(name):
-                    text = text.replace(" " + name, "")
-                else:
-                    text = text.replace(" " + name + " ", " ")
-
-        # --------------------------------------------------------------------------------------------------------------
-        if self.name_flag:
-            self.name_score += 1
-            for k, v in self.word_base['commands'].items():
-                if text in v:
-                    self.name_flag = False
-                    self.name_score = 0
-                    try:
-                        getattr(self, k[1])()
-                    except AttributeError:
-                        print("комманда распознанна, но не имеет функции")
-
-        if self.name_score == 3:
-            self.name_score = 0
-            self.name_flag = False
-
-        # --------------------------------------------------------------------------------------------------------------
     def model_recognition(self, text):
-        # if type(text) is str:
-            words = text.split()
-            if len(words) > 3:
-                intent = ["another", "None"]
-            else:
-                intent = self.model_pipeline.predict([text])[0]  # Предсказание метки
-                probs = self.model_pipeline.predict_proba([text])[0]
-                max_prob = np.max(probs)
-                if max_prob < 0.6:
-                    intent[0] = "another"
-            if intent[0] != "another":
-                print(intent[0], intent[1])
-                try:
-                    s = getattr(self, intent[0])(**eval(intent[1]))
-                    print(s, "\n\n")
-                except AttributeError as E:
-                    print("комманда распознанна, но не имеет функции\n", E)
-            else:
-                print(intent, "\n\n")
-        # elif type(text) is list:
-        #     intent = ["BaseFunction.another"]*len(text)
-        #     for i in range(len(text)):
-        #         if text[i] is not None:
-        #             self.words = text[i].split()
-        #             if len(self.words) > 3:
-        #                 intent[i] = "BaseFunction.another"
-        #             else:
-        #                 intent[i] = self.model_pipeline.predict([text[i]])[0]  # Предсказание метки
-        #                 probs = self.model_pipeline.predict_proba([text[i]])[0]
-        #                 max_prob = np.max(probs)
-        #                 if max_prob < 0.6:
-        #                     intent[i] = "BaseFunction.another"
-        #             print(intent, "\n\n")
-        #     intent = self.merge_text(intent)
-        #     intent = intent.split('.')
-        #     if intent[1] != "another":
-        #         try:
-        #             getattr(eval(intent[0]), intent[1])()
-        #         except AttributeError:
-        #             print("комманда распознанна, но не имеет функции")
-
+        words = text.split()
+        if len(words) > 3:
+            intent = ["another", "None"]
+        else:
+            intent = self.model_pipeline.predict([text])[0]  # Предсказание метки
+            probs = self.model_pipeline.predict_proba([text])[0]
+            max_prob = np.max(probs)
+            if max_prob < 0.6:
+                intent[0] = "another"
+        if intent[0] != "another":
+            print(intent[0], intent[1])
+            try:
+                s = getattr(self, intent[0])(**eval(intent[1]))
+                print(s, "\n\n")
+            except AttributeError as E:
+                print("комманда распознанна, но не имеет функции\n", E)
+        else:
+            print(intent, "\n\n")
--- Overlord.py
+++ Overlord.py
@@ -1,5 +1,5 @@
 from Base.Recognition import Recognition, Thread
-#from kivy.app import App
+# from kivy.app import App
 # from kivy.uix.screenmanager import ScreenManager
 # from kivy.core.window import Window
 # from Ui.Ui_test import Login
@@ -16,31 +16,14 @@ class Overlord(Recognition):
     def cycle(self):
         model_flag = None
         while True:
-            print('Использовать модель?')
-            while model_flag is None:
-                text = self.listen_one_msg()
-                if 'да' in text:
-                    model_flag = 1
-                    print('Система распознавания: модель')
-                elif 'нет' in text:
-                    model_flag = 0
-                    print('Система распознавания: словарь')
-
             try:
                 self.recognition_start(model_flag=model_flag)
                 print('Система "Overlord" готова к работе')
-                if model_flag == 0:
-                    for text in self.listen():
-                        print(f'>>> {text}')
-                        text = self.merge_text(text)
-                        print(f'>>> {text}')
-                        self.list_recognition(text)
-                elif model_flag == 1:
-                    for text in self.listen():
-                        print(f'>>> {text}')
-                        text = self.merge_text(text)
-                        print(f'>>> {text}')
-                        self.model_recognition(text)
+                for text in self.listen():
+                    print(f'>>> {text}')
+                    text = self.merge_text(text)
+                    print(f'>>> {text}')
+                    self.model_recognition(text)
 
             except OSError as E:
                 print(E)
